ed73e342d8a18ba7a32a4dd4b0684bcbb9365531
sravan-0110 <chitra.kumar2@s.amity.edu>
2025-07-07 15:31:00 +0530

    skills

--- sravan.c
+++ sravan.c
@@ -10,3 +10,8 @@ printf("👋 Introduction:\n");
     printf("I'm ChatGPT, an AI language model created by OpenAI.\n");
     printf("I can help with coding, writing, explaining concepts,\n");
     printf("translating languages, and much more!\n\n");
+     printf("📚 Skills:\n");
+    printf("- Understand and write code in many languages (like C, Python, Java)\n");
+    printf("- reading books\n");
+    printf("- playing cricket\n");
+    printf("-okay.\n\n");
